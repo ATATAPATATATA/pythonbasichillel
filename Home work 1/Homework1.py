85d2623136765548b66c79866cd988c36834d429
Ernest Sydorenkov <droppertrojan@gmail.com>
2022-08-03 23:14:10 +0300

    init

---
+++ Home work 1/Homework1.py
@@ -0,0 +1,7 @@
+import this #The Zen of Python, by Tim Peters
+
+import antigravity # open reference - https://xkcd.com/353/
+
+import __hello__ # in "Run" tab displays "Hello world!"
+
+from __future__ import braces #wrote error "SyntaxError: not a chance", it's an "easter eggs"
