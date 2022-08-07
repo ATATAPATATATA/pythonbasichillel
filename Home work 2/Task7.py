c273a6d1e0e42f7ca488ab1fe50aee1c4b470917
Ernest Sydorenkov <droppertrojan@gmail.com>
2022-08-07 18:27:11 +0300

    fix Task7.py

--- Home work 2/Task7.py
+++ Home work 2/Task7.py
@@ -1,2 +1,4 @@
-ten_of_stars = '*' * 10
-print(f'Ten stars {ten_of_stars}')
+print('Enter any digits to see magic: ', end='')
+stars = int(input())
+any_number_for_stars = '*' * stars
+print(f'{any_number_for_stars}')
